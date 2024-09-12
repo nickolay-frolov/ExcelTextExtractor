ed05d4f23334bf3379138541208f7ef6d7084e01
lordd <lordd@DESKTOP-DCH2T21>
2024-09-12 23:12:55 +0500

    Добавьте файлы проекта.

---
+++ ExtractorRibbon.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ExcelTextExtractor
+{
+    partial class ExtractorRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public ExtractorRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.ExtractBtn = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.ExtractBtn);
+            this.group1.Label = "Извлечь текст";
+            this.group1.Name = "group1";
+            // 
+            // ExtractBtn
+            // 
+            this.ExtractBtn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.ExtractBtn.Image = global::ExcelTextExtractor.Properties.Resources.copy_icon96;
+            this.ExtractBtn.Label = "Извлечь текст";
+            this.ExtractBtn.Name = "ExtractBtn";
+            this.ExtractBtn.ShowImage = true;
+            this.ExtractBtn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ExtractBtn_Click);
+            // 
+            // ExtractorRibbon
+            // 
+            this.Name = "ExtractorRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.ExtractorRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton ExtractBtn;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal ExtractorRibbon ExtractorRibbon
+        {
+            get { return this.GetRibbon<ExtractorRibbon>(); }
+        }
+    }
+}
---
+++ ExtractorRibbon.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelTextExtractor
+{
+    public partial class ExtractorRibbon
+    {
+        private void ExtractorRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void ExtractBtn_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                MergeAndCopyText();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void MergeAndCopyText()
+        {
+            Excel.Range selectedRange = Globals.ThisAddIn.Application.Selection;
+
+            if (selectedRange != null && selectedRange.Cells.Count > 1)
+            {
+                string mergedText = GetMergedText(selectedRange);
+                Excel.Range targetCell = selectedRange.Cells[selectedRange.Cells.Count].Offset[1, 0] as Excel.Range;
+
+                if (!string.IsNullOrEmpty(mergedText))
+                {
+                    targetCell.Value = mergedText;
+                    
+                    targetCell.WrapText = true;
+                    targetCell.HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;
+                    targetCell.VerticalAlignment = Excel.XlVAlign.xlVAlignTop;
+
+                    Clipboard.SetText(mergedText);
+                }
+                else
+                {
+                    MessageBox.Show("Выбранный диапазон не содержит текст.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите более одной ячейки.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private string GetMergedText(Excel.Range range)
+        {
+            StringBuilder mergedSB = new StringBuilder();
+
+            foreach (Excel.Range cell in range.Cells)
+            {
+                string cellText = cell.Text.Trim();
+
+                if (!string.IsNullOrEmpty(cellText))
+                {
+                    cellText = char.ToUpper(cellText[0]) + cellText.Substring(1);
+                }
+
+                if (!string.IsNullOrEmpty(cellText) && !cellText.EndsWith("."))
+                {
+                    cellText += ".";
+                }
+
+                mergedSB.Append(cellText + " ");
+            }
+
+            return mergedSB.ToString().TrimEnd();
+        }
+    }
+}
